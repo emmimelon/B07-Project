be94841667e0a10c2e9f815b2b08ff7c700579ac
emmimelon <emmimelon@gmail.com>
2023-11-10 23:16:14 -0500

    initial commit

---
+++ app/src/main/java/com/example/b07project/ui/post/PostViewModel.java
@@ -0,0 +1,11 @@
+package com.example.b07project.ui.post;
+
+import android.widget.TextView;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class PostViewModel extends ViewModel {
+    public PostViewModel() {
+    }
+}
