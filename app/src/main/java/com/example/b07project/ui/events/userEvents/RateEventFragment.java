97e132764908ba56234e42b9306a42a24a29374d
MichaelWK04 <michaelwalker1358@gmail.com>
2023-11-27 15:56:06 -0500

    Whoops i forgot to add the files :)

--- app/src/main/java/com/example/b07project/ui/events/userEvents/RateEventFragment.java
+++ app/src/main/java/com/example/b07project/ui/events/userEvents/RateEventFragment.java
@@ -1,2 +1,10 @@
-package com.example.b07project.ui.events.userEvents;public class RateEventFragment {
+package com.example.b07project.ui.events.userEvents;
+
+import androidx.fragment.app.Fragment;
+
+public class RateEventFragment extends Fragment {
+    private String title;
+    public RateEventFragment(String title){
+        this.title = title;
+    }
 }
